d9b6777d7ba95be6b94443b9f846c00fd2d2c027
justenpinto <justen.pinto@gmail.com>
2018-11-06 12:17:04 -0500

    Adding reverse and kth last node

--- interviewcake/linkedlist/linked_list.py
+++ interviewcake/linkedlist/linked_list.py
@@ -2,7 +2,8 @@ class LinkedList:
     def __init__(self, root):
         self.root = root
 
-    def delete_node(self, node_to_delete):
+    @staticmethod
+    def delete_node(node_to_delete):
         next_node = node_to_delete.next
 
         if next_node:
@@ -21,3 +22,51 @@ class LinkedList:
             if tortoise is hair:
                 return True
         return False
+
+    def reverse(self):
+        if not self.root:
+            return
+        current_node = self.root
+        previous_node = None
+        while current_node:
+            next_node = current_node.next
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+        self.root = previous_node
+
+    def nth_to_last_node(self, n):
+        leading_node = self.root
+        for i in range(n - 1):
+            leading_node = leading_node.next
+            if not leading_node:
+                raise Exception('Cannot find %dth item, length of list is too short (%d).' % (n, i))
+
+        current_node = self.root
+        while leading_node.next:
+            current_node = current_node.next
+            leading_node = leading_node.next
+
+        return current_node
+
+
+
+    def print_list(self):
+        current_node = self.root
+        values = []
+        while current_node:
+            values.append('%s' % current_node.value)
+            current_node = current_node.next
+        print('->'.join(values))
+
+if __name__ == '__main__':
+    from interviewcake.linkedlist.linked_list_node import LinkedListNode
+    a = LinkedListNode(1)
+    b = LinkedListNode(2)
+    c = LinkedListNode(3)
+
+    a.next = b
+    b.next = c
+
+    list = LinkedList(a)
+    print(list.nth_to_last_node(3))
--- interviewcake/linkedlist/linked_list_node.py
+++ interviewcake/linkedlist/linked_list_node.py
@@ -2,3 +2,6 @@ class LinkedListNode:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+    def __str__(self):
+        return '%s' % self.value
